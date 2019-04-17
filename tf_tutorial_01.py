0a1b58dcda6a3c4538739f2155d742f4c2d9fdf3
supermoonie <wangchao_moonie@163.com>
2019-04-17 21:21:45 +0800

    add tf_tutorial_01.py

---
+++ tf_tutorial_01.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+
+def var_demo():
+	a = tf.Variable(tf.linspace(-5., 5., 10), dtype=tf.float32)
+	b = tf.Variable(tf.random_normal([3, 3], mean=1.0, stddev=2.0, dtype=tf.float32), dtype=tf.float32)
+	c = tf.Variable(b.initialized_value(), dtype=tf.float32)
+	d = tf.Variable(tf.zeros([3, 3], dtype=tf.float32), dtype=tf.float32)
+	e = tf.assign(a, tf.linspace(-1., 1., 10))
+	f = tf.cast(e, dtype=tf.int32)
+	c1 = tf.constant(1)
+	c2 = tf.constant([2, 3])
+
+	init = tf.global_variables_initializer();
+	sess = tf.Session()
+	sess.run(init)
+	print(sess.run(a))
+	print(sess.run(b))
+	print(sess.run(c))
+	print(sess.run(d))
+	print(sess.run(e))
+	print(sess.run(f))
+	print(sess.run(c1))
+	print(sess.run(c2))
+
+
+def ops_demo():
+	a = tf.constant([[1, 2, 3], [4, 5, 6]])
+	b = tf.constant(4)
+	c = tf.add(a, b)
+	d = tf.multiply(a, b)
+	e = tf.divide(a, b)
+	f = tf.Variable(tf.random_normal([2, 3], 1.0, 3.0), dtype=tf.float32)
+	g = tf.add(f, tf.cast(tf.divide(a, b), dtype=tf.float32))
+	init = tf.global_variables_initializer();
+	sess = tf.Session()
+	sess.run(init)
+	print(sess.run(c))
+	print(sess.run(d))
+	print(sess.run(e))
+	print(sess.run(g))
+
+
+def mat_ops_demo():
+	m1 = tf.Variable(tf.random_normal([3, 3], 1.0, 3), dtype=tf.float32)
+	m2 = tf.Variable(tf.random_normal([3, 3], 3.0, 1), dtype=tf.float32)
+	m3 = tf.add(m1, m2)
+	m4 = tf.subtract(m1, m2)
+	m5 = tf.multiply(m1, m2)
+	m6 = tf.divide(m1, m2)
+	init = tf.global_variables_initializer();
+	sess = tf.Session()
+	sess.run(init)
+	print(sess.run([m3, m4, m5, m6]))
+
+
+mat_ops_demo()
